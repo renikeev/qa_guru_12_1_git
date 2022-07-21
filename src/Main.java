6015579ef2d3ccae063d68437dccafc84bef8010
Ruslan Enikeev <109678161+renikeev@users.noreply.github.com>
2022-07-21 16:17:16 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, QA.GURU!");
         System.out.println("Another Line");
-
     }
+
+    public static void another() {
+        System.out.println("Another Method");
+     }
 }
+
 
